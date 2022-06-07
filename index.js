514fbaf5a458f332e1f00991328eb12c9ba51055
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-06-07 04:34:25 +0300

    Done with the assignment

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+  if(someNumber<=400){
+    return 'This one is on me!';
+  }else if(someNumber>2000 && someNumber<2500){
+    return 'I will gladly take your thirty bucks.'
+  } else{
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city)
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  {return (city=="NYC" ? 'Ok, sounds good.' : 'No go.');}
+  
+function switchOnCharmFromTip(tip){
+    switch(tip) {
+      case "generous":
+        return("Thank you so much.")
+        break;
+      case "not as generous":
+        return("Thank you.")
+        break;
+      default:
+        return("Bye.")
+    }
+  }
+  
